643aabf203f70e1c2bead0894ef8221bd0836e00
Aidan Harris <mail@aidanharris.io>
2015-12-23 13:50:52 +0000

    Added eslint file (.eslintrc), modified .jshintrc and cleaned up some code in accordance with the rules specified by them (see https://github.com/airbnb/javascript)

--- feedparser/index.js
+++ feedparser/index.js
@@ -1,89 +1,93 @@
-var fs = require('fs');
-var parseString = require('xml2js').parseString;
+'use strict';
 
-var request = require('request');
-//require('request-debug')(request);
+const bitcannon = require('../src/bitcannon')();
 
-var zlib = require('zlib');
+const parseString = require('xml2js').parseString;
 
-function getGZipped(req, callback) {
-    var gunzip = zlib.createGunzip();
-    req.pipe(gunzip);
+const request = require('request');
+// require('request-debug')(request);
 
-    var buffer  = [];
-    gunzip.on('data', function (data) {
-        // decompression chunk ready, add it to the buffer
-        buffer.push(data);
-    }).on('end', function () {
-        //response and decompression complete, join the buffer and return
-        callback(null, JSON.parse(buffer));
-    }).on('error', function (e) {
-        callback(e);
-    });
+const zlib = require('zlib');
+
+function gunzip(body, callback) {
+  const buffer = new Buffer(body, 'base64');
+  zlib.unzip(buffer, function (err, buffer) {
+    if (!err) {
+      callback(err, buffer.toString());
+    } else {
+      bitcannon.error(err);
+      throw err;
+    }
+  });
 }
 
-function gunzip(body,callback) {
-    var buffer = new Buffer(body,'base64');
-    zlib.unzip(buffer, function(err, buffer) {
-        if (!err) {
-            callback(err,buffer.toString());
-        } else {
-            console.warn(err);
-        }
-    });
-}
-
-function parse(body) {
-    parseString(body, function(err, result) {
-        if(err) {
-            console.warn(err);
-            throw err;
-        }
-        var xmlns = '';
-        var torrentTag = false;
-        var torrentNameSpace = false;
-        try {
-            xmlns = result.rss.channel[0].torrent[0].$.xmlns; //<torrent xmlns="http://xmlns.ezrss.it/0.1/"><infoHash>...</infoHash></torrent>
-            torrentTag = true;
-        } catch (err) {
-            try {
-                xmlns = result.rss.$['xmlns:torrent']; //<rss xmlns:torrent="http://xmlns.ezrss.it/0.1/"><torrent:infoHash>...</torrent:infoHash></rss>
-                torrentNameSpace = true;
-            } catch (err) {
-                xmlns = result.rss.$['xmlns:atom']; //<rss xmlns:atom="http://www.w3.org/2005/Atom">...<enclosure url="http://example.com/example.torrent" type="application/x-bittorrent" length="10000"/>...</rss>
-            }
-        }
-        console.log(result.rss.channel[0]);
-        console.log('xmlns: ' + xmlns);
-        console.log(result.rss.channel[0].item.length);
-    });
+function parse(err, body) {
+  parseString(body, function (err, result) {
+    let xmlns = '';
+    let torrentTag = false;
+    let torrentNameSpace = false;
+    if (err) {
+      bitcannon.error(err);
+      throw err;
+    }
+    try {
+      // <torrent xmlns="http://xmlns.ezrss.it/0.1/">
+      //    <infoHash>...</infoHash>
+      // </torrent>
+      xmlns = result.rss.channel[0].torrent[0].$.xmlns;
+      torrentTag = true;
+    } catch (err) {
+      try {
+        // <rss xmlns:torrent="http://xmlns.ezrss.it/0.1/">
+        //    <torrent:infoHash>...</torrent:infoHash>
+        // </rss>
+        xmlns = result.rss.$['xmlns:torrent'];
+        torrentNameSpace = true;
+      } catch (err) {
+        // <rss xmlns:atom="http://www.w3.org/2005/Atom">
+        //    ...
+        //    <enclosure url="http://example.com/example.torrent"
+        //               type="application/x-bittorrent"
+        //               length="10000"
+        //    />
+        //    ...
+        // </rss>
+        xmlns = result.rss.$['xmlns:atom'];
+      }
+    }
+    if (torrentNameSpace || torrentTag) {
+      console.log(result.rss.channel[0]);
+      console.log('xmlns: ' + xmlns);
+      console.log(result.rss.channel[0].item.length);
+    }
+  });
 }
 
 request({
-    "uri": "URL TO PARSE",
-    "headers": {
-        "User-Agent": "BitCannon (http://bitcannon.io)",
-        "accept-encoding": "gzip"
-    },
-    "encoding": null,
-    "method": "GET",
-    "timeout": 10000,
-    "followRedirect": false,
-    "maxRedirects": 10,
-    "removeRefererHeader": true
-}, function(error, response, body) {
-    if(error) {
-        console.warn(error);
-        throw error;
-    }
-    switch (response.headers['content-encoding']) {
-        case 'gzip':
-            gunzip(body,function(err,body) {
-                parse(err,body);
-            });
-            break;
-        default:
-            parse(err,body);
-            break;
-    }
+  'uri': 'http://127.0.0.1:8000/rss.xml',
+  'headers': {
+    'User-Agent': 'BitCannon (http://bitcannon.io)',
+    'Accept-Encoding': 'gzip',
+  },
+  'encoding': null,
+  'method': 'GET',
+  'timeout': 10000,
+  'followRedirect': false,
+  'maxRedirects': 10,
+  'removeRefererHeader': true,
+}, function (error, response, body) {
+  if (error) {
+    bitcannon.error(error);
+    throw error;
+  }
+  switch (response.headers['content-encoding']) {
+    case 'gzip':
+      gunzip(body, function (err, body) {
+        parse(err, body);
+      });
+      break;
+    default:
+      parse(undefined, body);
+      break;
+  }
 });
--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -1,189 +1,208 @@
-"use strict";
-var bitcannon = require('../../bitcannon')();
-var express = require('express');
-var router = express.Router();
+'use strict';
+const bitcannon = require('../../bitcannon')();
+const express = require('express');
+const router = new express.Router();
 
-router.endpoints = [
-    '/browse',
-    '/stats',
-    '/browse/:category',
-    '/torrent/:btih',
-    '/search/:query',
-    '/search/:query/s/:skip',
-    '/search/:query/c/:category',
-    '/search/:query/c/:category/s/:skip',
-    '/scrape/:btih'
-];
-
-var api = require('../../bitcannon/api');
+const api = require('../../bitcannon/api');
 
 router.get('/', function (req, res, next) {
-   api.unsupported(req, res, next, function () {
-       res.json({status: 'OK'});
-   });
+  api.unsupported(req, res, next, function () {
+    res.json({ status: 'OK' });
+  });
 });
 
 router.get('/stats', function (req, res, next) {
-    api.unsupported(req, res, next, function () {
-        bitcannon.database.get.stats(function(err, count){
-            if(err) {
-                return res.status(500).type('json').send("{\"error\": \"" + require('statuses')[500] + "\"}");
-            } else {
-                res.json({
-                    "Count": count,
-                    "Trackers": bitcannon.config.trackers()
-                });
-            }
-        });
+  api.unsupported(req, res, next, function () {
+    bitcannon.database.get.stats(function (err, count) {
+      if (err) {
+        return res.status(500).type('json')
+          .send('{"error": "' + require('statuses')[500] + '"}');
+      }
+      res.json({
+        'Count': count,
+        'Trackers': bitcannon.config.trackers(),
+      });
     });
+  });
 });
 
 router.get('/browse', function (req, res, next) {
+  api.unsupported(req, res, next, function () {
+    bitcannon.database.get.categories(function (err, categories) {
+      if (err) {
+        return res.status(500).type('json')
+          .send('{"error": "' + require('statuses')[500] + '"}');
+      }
+      res.json(categories);
+    });
+  });
+});
+
+router.get('/browse/:category', function (req, res, next) {
+  api.unsupported(req, res, next, function () {
+    const limit = 200;
+    bitcannon.database.get.category(req.params.category, limit,
+      function (err, torrents) {
+        /*
+         To Do
+         * Find a better way of doing this calling String.replace multiple times
+         * on a large set of results could be expensive. Perhaps break the API
+         * by outputting the JSON keys in lowercase. This would mean going
+         * straight to version 2.0 of the API in accordance with semantic
+         * versioning (http://semver.org). Another option would be to go
+         * to version 1.1 of the API but keep compatibility with version 1.0
+         */
+        const torrentsJSON = JSON.stringify(torrents)
+          .replace(new RegExp('"_id":', 'g'), '"Btih":')
+          .replace(new RegExp('"category":', 'g'), '"Category":')
+          .replace(new RegExp('"details":', 'g'), '"Details":')
+          .replace(new RegExp('"imported":', 'g'), '"Imported":')
+          .replace(new RegExp('"lastmod":', 'g'), '"Lastmod":')
+          .replace(new RegExp('"size":', 'g'), '"Size":')
+          .replace(new RegExp('"swarm":', 'g'), '"Swarm":')
+          .replace(new RegExp('"leechers":', 'g'), '"Leechers":')
+          .replace(new RegExp('"seeders":', 'g'), '"Seeders":')
+          .replace(new RegExp('"title":', 'g'), '"Title":');
+        if (err) {
+          return res.status(500).type('json')
+            .send('{"error": "' + require('statuses')[500] + '"}');
+        }
+        res.json(JSON.parse(torrentsJSON));
+      });
+  });
+});
+
+router.get(['/browse/torrent/:btih', '/torrent/:btih'],
+  function (req, res, next) {
     api.unsupported(req, res, next, function () {
-        bitcannon.database.get.categories(function(err, categories){
-            if(err) {
-                return res.status(500).type('json').send("{\"error\": \"" + require('statuses')[500] + "\"}");
-            } else {
-                res.json(categories);
-            }
-        });
+      bitcannon.database.get.torrent(req.params.btih, function (err, torrent) {
+        /*
+         To Do
+         * As with /browse/:category need to find a better way of doing this.
+         */
+        const torrentJSON = JSON.stringify(torrent)
+          .replace(new RegExp('"_id":', 'g'), '"Btih":')
+          .replace(new RegExp('"category":', 'g'), '"Category":')
+          .replace(new RegExp('"details":', 'g'), '"Details":')
+          .replace(new RegExp('"imported":', 'g'), '"Imported":')
+          .replace(new RegExp('"lastmod":', 'g'), '"Lastmod":')
+          .replace(new RegExp('"size":', 'g'), '"Size":')
+          .replace(new RegExp('"swarm":', 'g'), '"Swarm":')
+          .replace(new RegExp('"leechers":', 'g'), '"Leechers":')
+          .replace(new RegExp('"seeders":', 'g'), '"Seeders":')
+          .replace(new RegExp('"title":', 'g'), '"Title":');
+        if (err) {
+          return res.status(500).type('json')
+            .send('{"error": "' + require('statuses')[500] + '"}');
+        }
+        return res.json(JSON.parse(torrentJSON));
+      });
     });
+  }
+);
+
+router.get('/scrape/:btih', function (req, res, next) {
+  api.unsupported(req, res, next, function () {
+    // If the id exists, i.e it is in the database
+    bitcannon.database.exists(req.params.btih, function (err, torrent) {
+      let record;
+      let recordID;
+
+      if (err) {
+        return res.status(500).type('json')
+          .send('{"error": "' + require('statuses')[500] + '"}');
+      }
+      if (torrent) {
+        bitcannon.scrape(req.params.btih, function (err, swarm) {
+          /* jshint ignore:start,-W101*/
+          res.json({
+            'Lastmod': (torrent[0].swarm.leechers !== swarm.Leechers || torrent[0].swarm.seeders !== swarm.Seeders) ? new Date().toISOString() : torrent[0].lastmod,
+            'Swarm': {
+              'Leechers': swarm.Leechers,
+              'Seeders': swarm.Seeders,
+            },
+          });
+          /* jshint ignore:end,+W101*/
+
+          record = torrent[0].toObject();
+          recordID = String(torrent[0]._id);
+          delete record._id;
+
+          // Modify leechers
+          if (torrent[0].swarm.leechers !== swarm.Leechers) {
+            bitcannon.log('Updating Leechers...');
+            record.swarm.leechers = swarm.Leechers;
+            record.lastmod = new Date().toISOString();
+            bitcannon.database.update.record(recordID, record);
+          }
+          // Modify seeders
+          if (torrent[0].swarm.seeders !== swarm.Seeders) {
+            bitcannon.log('Updating Seeders...');
+            record.swarm.seeders = swarm.Seeders;
+            record.lastmod = new Date().toISOString();
+            bitcannon.database.update.record(recordID, record);
+          }
+        });
+      } else {
+        return res.status(404).type('json')
+          .send('{"error": "' + require('statuses')[404] + '"}');
+      }
+    });
+  });
 });
 
-router.get('/browse/:category', function(req, res, next) {
+router.get(
+   ['/search/:query',
+    '/search/:query/s/:skip',
+    '/search/:query/c/:category',
+    '/search/:query/c/:category/s/:skip',
+   ],
+  function (req, res, next) {
+    req.params.skip = (req.params.skip === undefined) ? 0
+    : parseInt(req.params.skip, 10);
     api.unsupported(req, res, next, function () {
-        let limit = 200;
-        bitcannon.database.get.category(req.params.category,limit,function(err,torrents) {
-            /*
-            To Do
-                * Find a better way of doing this - calling String.replace multiple times on a large set of results could be expensive
-                    * Perhaps break the API by outputting the JSON keys in lowercase
-                    * This would mean going straight to version 2.0 of the API in accordance with semantic versioning (http://semver.org)
-                    * Another option would be to go to version 1.1 of the API but keep compatibility with version 1.0
-             */
-            torrents = JSON.stringify(torrents)
-                .replace(new RegExp('"_id":','g'),'"Btih":')
-                .replace(new RegExp('"category":','g'),'"Category":')
-                .replace(new RegExp('"details":','g'),'"Details":')
-                .replace(new RegExp('"imported":','g'),'"Imported":')
-                .replace(new RegExp('"lastmod":','g'),'"Lastmod":')
-                .replace(new RegExp('"size":','g'),'"Size":')
-                .replace(new RegExp('"swarm":','g'),'"Swarm":')
-                .replace(new RegExp('"leechers":','g'),'"Leechers":')
-                .replace(new RegExp('"seeders":','g'),'"Seeders":')
-                .replace(new RegExp('"title":','g'),'"Title":');
-                if(err) {
-                    return res.status(500).type('json').send("{\"error\": \"" + require('statuses')[500] + "\"}");
-                } else {
-                    res.json(JSON.parse(torrents));
-                }
-        });
+      bitcannon.database.get.search(
+        req.params.query,
+        req.params.category,
+        req.params.skip,
+        function (err, torrents) {
+          if (err) {
+            return res.status(500).type('json')
+              .send('{"error": "' + require('statuses')[500] + '"}');
+          }
+          if (torrents.length === 0) {
+            return res.status(404).type('json')
+              .send('{"error": "' +
+                'No results found for ' + req.params.query + '"}');
+          }
+          const torrentsJSON = JSON.stringify(torrents)
+            .replace(new RegExp('"_id":', 'g'), '"Btih":')
+            .replace(new RegExp('"category":', 'g'), '"Category":')
+            .replace(new RegExp('"details":', 'g'), '"Details":')
+            .replace(new RegExp('"imported":', 'g'), '"Imported":')
+            .replace(new RegExp('"lastmod":', 'g'), '"Lastmod":')
+            .replace(new RegExp('"size":', 'g'), '"Size":')
+            .replace(new RegExp('"swarm":', 'g'), '"Swarm":')
+            .replace(new RegExp('"leechers":', 'g'), '"Leechers":')
+            .replace(new RegExp('"seeders":', 'g'), '"Seeders":')
+            .replace(new RegExp('"title":', 'g'), '"Title":');
+          return res.json(JSON.parse(torrentsJSON));
+        }
+      );
     });
-});
+  }
+);
 
-router.get(['/browse/torrent/:btih','/torrent/:btih'], function(req, res, next) {
-    api.unsupported(req, res, next, function () {
-        bitcannon.database.get.torrent(req.params.btih,function(err,torrent) {
-            /*
-            To Do
-                * As with /browse/:category need to find a better way of doing this.
-             */
-            torrent = JSON.stringify(torrent)
-                .replace(new RegExp('"_id":','g'),'"Btih":')
-                .replace(new RegExp('"category":','g'),'"Category":')
-                .replace(new RegExp('"details":','g'),'"Details":')
-                .replace(new RegExp('"imported":','g'),'"Imported":')
-                .replace(new RegExp('"lastmod":','g'),'"Lastmod":')
-                .replace(new RegExp('"size":','g'),'"Size":')
-                .replace(new RegExp('"swarm":','g'),'"Swarm":')
-                .replace(new RegExp('"leechers":','g'),'"Leechers":')
-                .replace(new RegExp('"seeders":','g'),'"Seeders":')
-                .replace(new RegExp('"title":','g'),'"Title":');
-                if(err) {
-                    return res.status(500).type('json').send("{\"error\": \"" + require('statuses')[500] + "\"}");
-                } else {
-                    return res.json(JSON.parse(torrent));
-                }
-        });
-    });
-});
-
-router.get('/scrape/:btih', function(req, res, next) {
-    api.unsupported(req, res, next, function() {
-        //If the id exists, i.e it is in the database
-        bitcannon.database.exists(req.params.btih,function(err,torrent) {
-            if(err) {
-                return res.status(500).type('json').send("{\"error\": \"" + require('statuses')[500] + "\"}");
-            }
-            if(torrent) {
-                bitcannon.scrape(req.params.btih, function (err, swarm) {
-                    res.json({
-                        "Lastmod": ((torrent[0].swarm.leechers !== swarm.Leechers || torrent[0].swarm.seeders !== swarm.Seeders) ? new Date().toISOString() : torrent[0].lastmod),
-                        "Swarm": {
-                            "Leechers": swarm.Leechers,
-                            "Seeders": swarm.Seeders
-                        }
-                    });
-
-                    var record = torrent[0].toObject();
-                    let recordID = String(torrent[0]._id);
-                    delete record._id;
-
-                    //Modify leechers
-                    if(torrent[0].swarm.leechers !== swarm.Leechers) {
-                        bitcannon.log('Updating Leechers...');
-                        record.swarm.leechers = swarm.Leechers;
-                        record.lastmod = new Date().toISOString();
-                        bitcannon.database.update.record(recordID,record);
-                    }
-                    //Modify seeders
-                    if(torrent[0].swarm.seeders !== swarm.Seeders) {
-                        bitcannon.log('Updating Seeders...');
-                        record.swarm.seeders = swarm.Seeders;
-                        record.lastmod = new Date().toISOString();
-                        bitcannon.database.update.record(recordID,record);
-                    }
-                });
-            } else {
-                return res.status(404).type('json').send("{\"error\": \"" + require('statuses')[404] + "\"}");
-            }
-        });
-    });
-});
-
-router.get(['/search/:query','/search/:query/s/:skip','/search/:query/c/:category','/search/:query/c/:category/s/:skip'], function(req, res, next) {
-    req.params.skip = (req.params.skip === undefined) ? 0 : parseInt(req.params.skip);
-    api.unsupported(req, res, next, function(){
-        bitcannon.database.get.search(req.params.query,req.params.category,req.params.skip,function(err,torrents) {
-           if(err) {
-               return res.status(500).type('json').send("{\"error\": \"" + require('statuses')[500] + "\"}");
-           } else {
-               if(torrents.length === 0) {
-                   return res.status(404).type('json').send("{\"error\": \"" + "No results found for " + req.params.query + "\"}");
-               }
-               torrents = JSON.stringify(torrents)
-                   .replace(new RegExp('"_id":','g'),'"Btih":')
-                   .replace(new RegExp('"category":','g'),'"Category":')
-                   .replace(new RegExp('"details":','g'),'"Details":')
-                   .replace(new RegExp('"imported":','g'),'"Imported":')
-                   .replace(new RegExp('"lastmod":','g'),'"Lastmod":')
-                   .replace(new RegExp('"size":','g'),'"Size":')
-                   .replace(new RegExp('"swarm":','g'),'"Swarm":')
-                   .replace(new RegExp('"leechers":','g'),'"Leechers":')
-                   .replace(new RegExp('"seeders":','g'),'"Seeders":')
-                   .replace(new RegExp('"title":','g'),'"Title":');
-               return res.json(JSON.parse(torrents));
-           }
-        });
-    });
-});
-
-//Catch all
-router.get(/^(.*)$/, function(req, res, next){
-    //All routers must call api.unsupported with a callback function in order to verify if the API version trying to be used is valid
-    api.unsupported(req, res, next, function() {
-        return res.status(400).type('json').send("{\"error\": \"" + require('statuses')[400] + "\"}"); //If the version number is okay we send a 400 'Bad Request' because the request is malformed
-    });
+// Catch all
+router.get(/^(.*)$/, function (req, res, next) {
+  // All routers must call api.unsupported with a callback function in
+  // order to verify if the API version trying to be used is valid.
+  api.unsupported(req, res, next, function () {
+    // If the version number is okay we send a 400 'Bad Request'
+    // because the request is malformed.
+    return res.status(400).type('json')
+      .send('{"error": "' + require('statuses')[400] + '"}');
+  });
 });
 
 module.exports = router;
