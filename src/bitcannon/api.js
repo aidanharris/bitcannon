205f917ac356ff551f9e759ffcf2c0b5c0009d2e
Aidan Harris <mail@aidanharris.io>
2016-01-09 21:50:19 +0000

    Make constants uppercase in 'src/bitcannon/api.js' in accordance with convention

--- src/bitcannon/api.js
+++ src/bitcannon/api.js
@@ -2,12 +2,12 @@
 
 module.exports = (function () {
   // The latest API version
-  const latestApi = 1.0;
+  const LATEST_API = 1.0;
 
   // Old versions of the API we no longer support
   // - these need to get sent a HTTP 410 - Gone
   // header along with a message to update to the latest API
-  const legacyVersions = 0.9;
+  const LEGACY_VERSIONS = 0.9;
 
   return {
     unsupported(req, res, next, callback) {
@@ -18,16 +18,16 @@ module.exports = (function () {
       );
       // If it's less than or equal to legacyVersions we send a HTTP 410 'Gone'
       // error along with a message explaining that a deprecated API is used
-      if (req.apiVersion <= legacyVersions) {
+      if (req.apiVersion <= LEGACY_VERSIONS) {
         return res.status(410).type('json').json({
-          'error':
+          error:
           'You are trying to use a deprecated version of the BitCannon API!' +
           ' Please upgrade to the latest version ' +
           '(see https://github.com/aidanharris/bitcannon/wiki/API)' });
       }
       // If it's greater than the current api version we send a HTTP 400
       // 'Bad Request' error because the API version does not exist
-      if (req.apiVersion > latestApi) {
+      if (req.apiVersion > LATEST_API) {
         return res.status(400).type('json')
           .json({ error: require('../server/node_modules/statuses')[400] });
       }
