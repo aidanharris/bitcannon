3486e2b5c6bc4526481f0cb3cb1b5ad5de000570
Aidan Harris <mail@aidanharris.io>
2016-01-23 17:27:18 +0000

    Add 'bitcannon-migrate.js' module which will manage upgrades and migrations between versions

---
+++ src/bitcannon/bitcannon-migrate.js
@@ -0,0 +1,8 @@
+/**
+  # BitCannon upgrading and migration
+ */
+
+'use strict';
+
+// Get the current version number from the package.json
+const CURRENT_VERSION = require(__dirname + '/package.json').version;
